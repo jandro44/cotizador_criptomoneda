00c2275eb0f04c93c22ce042bd8f2f7cd1bafa25
Alejandro <castropintoale@gmail.com>
2023-03-05 05:05:53 -0300

    primer commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos' },
+    { id: 'CLP', nombre: 'Peso Chileno' },
+    { id: 'MXN', nombre: 'Peso Mexicano' },
+    { id: 'EUR', nombre: 'Euro' },
+];
+
+export {
+    monedas
+}
